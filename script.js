c96719911f05cb37d6dfb9294dc780dd2bab5224
bhavnachauhann <164909808+bhavnachauhann@users.noreply.github.com>
2024-04-18 15:50:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// var song = document.getElementById("song");
+// var icon = document.getElementById("icon");
+
+// icon.onclick = function(){
+//     if(song.paused){
+//         song.play();
+//         icon.src = "pause.png";
+//     }
+//     else{
+//         song.pause();
+//         icon.src = "play.jpeg";
+//     }
+// }
+
+var song=document.getElementById("song");
+var icon =document.getElementById("icon");
+
+icon.onclick= function(){
+    if(song.paused){
+        song.play();
+        icon.src="pause.png"
+    }
+    else{
+        song.pause();
+        icon.src="play.jpeg"
+    }
+}
+ 
+
+    
